7e824217ff248c37ed922c30f798c7e0a28c0d6c
rramname <rramname@ces-ltd.com>
2018-08-23 15:11:24 -0400

    app.js

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 var http=require('http')
+
+function build(){
+    
+    var exec = require('child_process').exec,child;
+
+child = exec('ng build', function(error,stdout,stderr) { 
+    if (error) {
+      console.log(error.stack); 
+      //console.log('Error code: '+ error.code); 
+      //console.log('Signal received: '+ 
+      //       error.signal);
+      }
+      //console.log('Child Process stdout: '+ stdout);
+      //console.log('Child Process stderr: '+ stderr);
+  });
+} 
+build();
+
 const express = require('express');
 const cors = require('cors');
 
